caecfc55b7b7ff2b68f5ab53b016bbed45af4b4b
WonderBoi99 <61436662+WonderBoi99@users.noreply.github.com>
2024-02-06 15:10:17 -0700

    motified methods to handle collisions by chaining approch

--- data-structures/hash_map.py
+++ data-structures/hash_map.py
@@ -2,8 +2,9 @@ class HashMap():
     def __init__(self, size=100):
         #uses linked lists in background not list
         #allocates space on demand, better storage
-        self.MAX = 20
-        self.storage = [None for i in range(self.MAX)]
+        self.MAX = 10
+        #for collison handling, using chaining solution
+        self.storage = [[] for i in range(self.MAX)]
 
     def hashFunction(self, key):
         #applies custom hash function on key, to get a index for the storage list
@@ -14,29 +15,42 @@ class HashMap():
     
     def __setitem__(self, key, value):
         index = self.hashFunction(key)
-        self.storage[index] = value
+        found = False
+        for i, pair in enumerate(self.storage[index]):
+            if len(pair) == 2 and pair[0] == key:
+                found = True
+                self.storage[index][i] = (key,value)
+        if not found:
+            self.storage[index].append((key, value))
+
+
 
     def __getitem__(self, key):
         index = self.hashFunction(key)
-        return self.storage[index]
+        for i, pair in enumerate(self.storage[index]):
+            if len(pair) == 2 and pair[0] == key:
+                return self.storage[index][i]
     
     def __delitem__(self, key):
         index = self.hashFunction(key)
-        self.storage[index] = None
-
+        for i, pair in enumerate(self.storage[index]):
+            if len(pair) == 2 and pair[0] == key:
+                self.storage[index][i] = None
 
     def show(self):
-        for value in self.storage:
-            print(value)
+        for list in self.storage:
+            print(list)
 
 if __name__ == '__main__':
     test = HashMap()
     test['jan 2'] = 100
-    test['mar 5'] = 3400
-    test['dec 12'] = 'tres'
-    print(test['mar 5'])
     test.show()
     print('-------')
-    del test['dec 12']
-    # print(test.hashFunction('bobby'))
+    test['jan 2'] = 500
+    print(test['jan 2'])
     test.show()
+    print('-------')
+    del test['jan 2']
+    test.show()
+    print('-------')
+    
