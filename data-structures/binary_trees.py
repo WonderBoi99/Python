941a19faa2f15d75d94e8e7438f47bb3fb5f7268
WonderBoi99 <61436662+WonderBoi99@users.noreply.github.com>
2024-02-06 23:05:47 -0700

    practise on pre/post traversal

--- data-structures/binary_trees.py
+++ data-structures/binary_trees.py
@@ -45,6 +45,42 @@ class BinaryTreeNode:
 
         return e
     
+    def find_min(self):
+        if self.left:
+            return self.left.find_min()
+        else:
+            return self.data
+        
+    def find_max(self):
+        if self.right:
+            return self.right.find_max()
+        else:
+            return self.data
+        
+    def pre_order_traversal(self):
+        e = []
+        e.append(self.data)
+        if self.left:
+            e += self.left.pre_order_traversal()
+        if self.right:
+            e += self.right.pre_order_traversal()
+
+        return e
+    
+    def post_order_traversal(self):
+        e = []
+        
+        if self.left:
+            e += self.left.post_order_traversal()
+        if self.right:
+            e += self.right.post_order_traversal()
+        e.append(self.data)
+        return e
+    
+    def calculate_sum(self):
+        values = self.in_order_traversal()
+        return sum(values)
+
 def build_tree(values):
     root = BinaryTreeNode(values[0])
     for val in range(1,len(values)):
@@ -55,5 +91,9 @@ if __name__ == '__main__':
     test = build_tree([15,12,7,14,27,20,88,23])
     print(test.in_order_traversal())
     print(test.search(15))
-        
+    print(test.find_min())
+    print(test.find_max())
+    print(test.calculate_sum())
+    print(test.pre_order_traversal())
+    print(test.post_order_traversal())
         
