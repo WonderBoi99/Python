ad1a6c480ba1b3a242c72e267f418bedcd094c67
WonderBoi99 <61436662+WonderBoi99@users.noreply.github.com>
2024-02-07 00:53:53 -0700

    learning about graph, still need a lot more practise on this dt

---
+++ data-structures/directed_graph.py
@@ -0,0 +1,81 @@
+class Graph:
+    def __init__(self, edges):
+        self.edges = edges
+        self.graph_dict = {}
+        for start, end in edges:
+            if start in self.graph_dict:
+                self.graph_dict[start].append(end)
+            else:
+                self.graph_dict[start] = [end]
+        print("Graph Dict:", self.graph_dict)
+    
+    def get_paths(self, start, end, path=[]):
+        path = path + [start]
+
+        if start == end:
+            return [path]
+        
+        if start not in self.graph_dict:
+            return []
+        
+        new_paths = []
+        for node in self.graph_dict[start]:
+            if node not in path:
+                new_path = self.get_paths(node,end,path)
+                for p in new_path:
+                    new_paths.append(p)
+
+        return new_paths
+    
+    def get_shortest_path(self, start, end, path=[]):
+        path = path + [start]
+
+        if start == end:
+            return [path]
+        
+        if start not in self.graph_dict:
+            return None
+        
+        shortest_path = None
+        for node in self.graph_dict[start]:
+            if node not in path:
+                short_path = self.get_shortest_path(node,end,path)
+                if short_path:
+                    if shortest_path is None or len(short_path) < len(shortest_path):
+                        shortest_path = short_path
+
+        return shortest_path
+
+
+
+
+if __name__ == '__main__':
+
+    routes = [
+        ("Mumbai","Pune"),
+        ("Mumbai", "Surat"),
+        ("Surat", "Bangaluru"),
+        ("Pune","Hyderabad"),
+        ("Pune","Mysuru"),
+        ("Hyderabad","Bangaluru"),
+        ("Hyderabad", "Chennai"),
+        ("Mysuru", "Bangaluru"),
+        ("Chennai", "Bangaluru")
+    ]
+
+    routes = [
+        ("Mumbai", "Paris"),
+        ("Mumbai", "Dubai"),
+        ("Paris", "Dubai"),
+        ("Paris", "New York"),
+        ("Dubai", "New York"),
+        ("New York", "Toronto"),
+    ]
+
+    route_graph = Graph(routes)    
+
+    start = "Mumbai"
+    end = "New York"
+
+    print(f"All paths between: {start} and {end}: ",route_graph.get_paths(start,end))
+    print(f"Shortest path between {start} and {end}: ", route_graph.get_shortest_path(start,end))
