c0bf75de2b4725e44b60643a2a401645eb808289
WonderBoi99 <61436662+WonderBoi99@users.noreply.github.com>
2024-02-07 16:01:04 -0700

    writing method in recursive

--- algorithms/binary_search.py
+++ algorithms/binary_search.py
@@ -1,7 +1,7 @@
 
 
 
-def binary_search_recursive(numbers_list, number_to_find):
+def binary_search(numbers_list, number_to_find):
     left_index = 0
     right_index = len(numbers_list) - 1
     mid_index = 0
@@ -19,6 +19,22 @@ def binary_search_recursive(numbers_list, number_to_find):
 
     return -1
 
+def binary_search_recursive(numbers_list, number_to_find, left_index, right_index):
+
+    if left_index <= right_index:
+        mid_index = (left_index + right_index) // 2
+        mid_number = numbers_list[mid_index]
+        if mid_number == number_to_find:
+            return mid_index
+        if mid_number < number_to_find:
+            left_index = mid_index + 1
+        else:
+            right_index = mid_index - 1
+
+        return binary_search_recursive(numbers_list, number_to_find, left_index, right_index)
+    else:
+        return -1
+
     
 
 
@@ -27,5 +43,7 @@ if __name__ == '__main__':
     numbers_list = [12, 15, 17, 19, 21, 24, 45, 67]
     number_to_find = 21
 
-    index = binary_search_recursive(numbers_list, number_to_find, 0, len(numbers_list))
+    index = binary_search(numbers_list, number_to_find)
+    print(f"Number found at index {index} using binary search")
+    index = binary_search_recursive(numbers_list, 12, 0, len(numbers_list)-1)
     print(f"Number found at index {index} using binary search")
