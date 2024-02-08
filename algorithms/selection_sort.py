3dfc43a6ae2dd6b06cd274e4ce64524662c6b9bb
WonderBoi99 <61436662+WonderBoi99@users.noreply.github.com>
2024-02-08 13:14:43 -0700

    learning selection sort

---
+++ algorithms/selection_sort.py
@@ -0,0 +1,34 @@
+def selection_sort(elements):
+    size = len(elements)
+
+    for i in range(0, size):
+        min_index = i
+        for j in range(min_index+1, size):
+            if elements[j] < elements[min_index]:
+                min_index = j
+        
+        if i != min_index:
+            temp = elements[min_index]
+            elements[min_index] = elements[i]
+            elements[i] = temp
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    tests = [
+        [89, 78, 61, 53, 23, 21, 17, 12, 9, 7, 6, 2, 1],
+        [],
+        [1,5,8,9],
+        [234,3,1,56,34,12,9,12,1300],
+        [78, 12, 15, 8, 61, 53, 23, 27],
+        [5]
+    ]
+    for elements in tests:
+        selection_sort(elements)
+        print(elements)
