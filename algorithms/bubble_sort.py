34d2badc08b769a517b7b283235dc0fff7569a94
WonderBoi99 <61436662+WonderBoi99@users.noreply.github.com>
2024-02-08 12:02:28 -0700

    practise

--- algorithms/bubble_sort.py
+++ algorithms/bubble_sort.py
@@ -1,11 +1,11 @@
 
-def bubble_sort(elements):
+def bubble_sort(elements, key='transaction_amount'):
     size = len(elements)
 
     for j in range(size-1):
         swapped = False
         for i in range(size-1-j):
-            if elements[i] > elements[i+1]:
+            if elements[i][key] > elements[i+1][key]:
                 temp = elements[i]
                 elements[i] = elements[i+1]
                 elements[i+1] = temp
@@ -15,9 +15,15 @@ def bubble_sort(elements):
 
 
 if __name__ == '__main__':
-    elements = [5,9,2,1,67,34,88,34]
-    elements = [1,2,3,4,2]
-    elements = ["mona", "dhaval", "aamir", "tina", "chang"]
+    # elements = [5,9,2,1,67,34,88,34]
+    # elements = [1,2,3,4,2]
+    # elements = ["mona", "dhaval", "aamir", "tina", "chang"]
+    elements = [
+        { 'name': 'mona',   'transaction_amount': 1000, 'device': 'iphone-10'},
+        { 'name': 'dhaval', 'transaction_amount': 400,  'device': 'google pixel'},
+        { 'name': 'kathy',  'transaction_amount': 200,  'device': 'vivo'},
+        { 'name': 'aamir',  'transaction_amount': 800,  'device': 'iphone-8'},
+    ]
 
-    bubble_sort(elements)
+    bubble_sort(elements, 'name')
     print(elements)
